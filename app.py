c86e9daf18c61fa13b8ac7008d46a6d20af71ea5
BIDISHA-CHANDA <67823051+BIDISHA-CHANDA@users.noreply.github.com>
2021-02-23 21:21:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('heart_final.pkl', 'rb'))
+@app.route('/')
+def home():
+  return render_template('HEART.html')
+
+
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1, 38)
+    loaded_model = pickle.load(open("diab_final.pkl", "rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+        result = ValuePredictor(to_predict_list)
+        if int(result) == 1:
+            prediction = 'Has chances of heart disease'
+        else:
+            prediction = 'Has a healthy heart'
+        return render_template("result.html", prediction=prediction)
+
+
+if __name__ == '__main__':
+    app.run()
